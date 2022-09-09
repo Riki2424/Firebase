8e13c0d928c93489a833c78441cab7fef2eea0c4
Riki <105851221+Riki2424@users.noreply.github.com>
2022-09-09 16:45:51 +0900

    feature

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { storage } from "./firebase";
 import { ref, getDownloadURL, listAll } from "firebase/storage";
 import { useState, useEffect } from "react";
-import { db } from "./firebase"
+// import { db } from "./firebase"
 // import { collection, getDocs, onSnapshot } from "firebase/firestor";
 
 function App() {
