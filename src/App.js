78da73166180fd74ca24be87e1c32bf11d2102cc
Riki <105851221+Riki2424@users.noreply.github.com>
2022-09-09 16:25:29 +0900

    5行目変更

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { storage } from "./firebase";
 import { ref, getDownloadURL, listAll } from "firebase/storage";
 import { useState, useEffect } from "react";
 import { db } from "./firebase"
-import { collection, getDocs, onSnapshot } from "firebase/firestore";
+import { collection, getDocs, onSnapshot } from "firebase/";
 
 function App() {
   //useStateで、データをブラウザに表示
